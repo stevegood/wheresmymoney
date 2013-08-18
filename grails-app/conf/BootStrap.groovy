5f09d891106800c8ea98459fcd062dfff1351168
Steve Good <steve@stevegood.org>
2013-08-18 14:36:36 -0500

    Created some basic bootstrap data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,16 @@
+import org.stevegood.sec.Role
+import org.stevegood.sec.User
+import org.stevegood.sec.UserRole
+
 class BootStrap {
 
     def init = { servletContext ->
+        if (User.count() == 0) {
+            // setup the application
+            User admin = new User(username: 'admin', password: 'admin', enabled: true).save(flush: true, insert: true)
+            Role roleSuperAdmin = new Role(authority: 'ROLE_SUPER_ADMIN').save(flush: true, insert: true)
+            UserRole.create(admin, roleSuperAdmin)
+        }
     }
     def destroy = {
     }
